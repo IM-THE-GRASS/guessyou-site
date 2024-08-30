45309947674bfd9bf5061846ebfa62a40e14c14c
IM-THE-GRASS <thegrassmanreplit@gmail.com>
2024-08-30 00:56:01 -0600

    aait says the country now

--- guessyou/pages/results.py
+++ guessyou/pages/results.py
@@ -1,26 +1,53 @@
 import reflex as rx
 from guessyou.components.card import card   
+from guessyou.state import State
 
-
-
+@rx.page(on_load=State.load_result)
 def results() -> rx.Component:
     return rx.box(
         rx.vstack(
-            rx.text(
-                "Hi, Josh 👋",
-                font_weight="bold",
-                line_height="134px",
-                font_size="96px"
+            rx.hstack(
+                rx.text(
+                    "Hi, ",
+                    font_weight="bold",
+                    line_height="134px",
+                    font_size="96px"
+                ),
+                rx.text(
+                    State.current_input,
+                    font_weight="bold",
+                    line_height="134px",
+                    font_size="96px"
+                ),
+                rx.text(
+                    " 👋",
+                    font_weight="bold",
+                    line_height="134px",
+                    font_size="96px"
+                ),
             ),
+            
             rx.text(
                 "I think you are:",
                 font_size="48px",
                 line_height="48px"
             ),
             rx.hstack(
-                card(),
-                card(),
-                card(),
+                card(
+                    image=State.nation_flag,
+                    text=State.nation_text,
+                    subtext=State.nation_subtext    
+                ),
+                card(
+                    image=State.gender_img,
+                    text=State.gender_text,
+                    subtext=State.gender_subtext    
+                ),
+                card(
+                    image=State.age_img,
+                    text=State.age_text,
+                    subtext=State.age_subtext    
+                ),
                 spacing="0",
                 gap="198px",
                 width="1821px",
--- guessyou/state.py
+++ guessyou/state.py
@@ -1,8 +1,31 @@
 import reflex as rx
-
+import requests
 
 class State(rx.State):
     current_input:str
+    
+    nation_flag:str
+    nation_text:str
+    nation_subtext:str
+    
+    gender_img:str
+    gender_text:str
+    gender_subtext:str
+    
+    age_img:str
+    age_text:str
+    age_subtext:str
+    
+    
     def on_change_input(self, new):
         self.current_input = new
+    def load_result(self):
+        nation = requests.get(f"https://api.nationalize.io/?name={self.current_input}")
+        nation_data = nation.json()
+        nation_id = nation_data["country"][0]["country_id"]
+        nation_info = requests.get(f"https://restcountries.com/v3.1/alpha/{nation_id}").json()
+        nation_name = nation_info[0]["name"]["common"]
+        self.nation_text = nation_name
+        self.nation_subtext = nation_data["country"][0]["probability"] * 100
+        self.nation_subtext = f"{round(self.nation_subtext)}% sure"
         
